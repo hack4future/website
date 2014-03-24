93faa21b4c85b331081c221a2eb3b2d7d9a6b17b
Igor Kandyba <igor.kandyba@gmail.com>
2014-03-24 19:36:56 +0300

    Configured the deployment script

--- web/deploy.php
+++ web/deploy.php
@@ -16,6 +16,7 @@
     'git submodule sync',
     'git submodule update',
     'git submodule status',
+    'php app/console cache:clear --env=prod --no-debug',
   );
 
   // Run the commands for output
@@ -34,14 +35,14 @@
 <html lang="en-US">
 <head>
   <meta charset="UTF-8">
-  <title>GIT DEPLOYMENT SCRIPT</title>
+  <title>Deploying Hackathon site...</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
  .  ____  .    ____________________________
  |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script |
+ |___==___|  /               |
               |____________________________|
 
 <?php echo $output; ?>
